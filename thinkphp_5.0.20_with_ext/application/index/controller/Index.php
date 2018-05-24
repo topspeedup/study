a53a2621fdd3e798f018f4ab4ea06b9436a28a17
topspeedup <491364922@qq.com>
2018-05-24 22:05:16 +0800

    study

---
+++ thinkphp_5.0.20_with_ext/application/index/controller/Index.php
@@ -0,0 +1,21 @@
+<?php
+namespace app\index\controller;
+use think\Db;
+
+class Index
+{
+    public function index()
+    {
+		$where = [
+			'内容' => 'www.baidu.com'
+		];
+		$res = Db::name('data_content_263')->where($where)->value('PageUrl');
+		var_dump($res);
+    }
+	
+	public function direc(){
+		$str = input('post.str',false);
+		$data = explode("\n",$str);
+		var_dump($data);
+	}
+}
